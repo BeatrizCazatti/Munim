4d02be6d2ca18a53ea9c96f9a3ddb0ea077bc052
Beatriz Cazatti Figueiredo <beatrizcazatti@gmail.com>
2026-09-12 08:58:38 -0300

    retira código para iOS

--- Munim/Munim/Shared/AdaptiveTextStyle.swift
+++ Munim/Munim/Shared/AdaptiveTextStyle.swift
@@ -12,21 +12,6 @@ enum AppTextStyle {
     case caption
     case caption2
 
-    var iOS: Font {
-        switch self {
-        case .largeTitle: .largeTitle
-        case .title: .title
-        case .title2: .title2
-        case .title3: .title3
-        case .headline: .headline
-        case .subheadline: .subheadline
-        case .body: .body
-        case .callout: .callout
-        case .caption: .caption
-        case .caption2: .caption2
-        }
-    }
-
     var macOSBaseSize: CGFloat {
         switch self {
         case .largeTitle: 28
